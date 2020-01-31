e85c4f45d4d4e66e26fa5ca174b1f438f484ed6a
Arkadeep Basu <58461095+xanther-olog@users.noreply.github.com>
2020-01-31 21:40:41 +0530

    hide api key

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,9 @@ import 'firebase/firebase-auth'
 import 'firebase/storage'
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyAONNfLZ6gLk8kQaoLj10hzflyxUvCtItw",
-    authDomain: "login-microservice-ammonation.firebaseapp.com",
-    databaseURL: "https://login-microservice-ammonation.firebaseio.com",
+    apiKey: "-",
+    authDomain: "-",
+    databaseURL: "-",
     projectId: "login-microservice-ammonation",
     storageBucket: "login-microservice-ammonation.appspot.com",
     messagingSenderId: "084539004734"
